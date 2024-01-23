819f6f41a53d721016797d6e89bd820d38ba4f76
Linh Dang <dangdiemlinh04@gmail.com>
2024-01-23 14:10:31 -0500

    Initial Commit

---
+++ buildnetflix/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  buildnetflix
+//
+//  Created by Linh Dang on 1/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ buildnetflix/buildnetflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  buildnetflixApp.swift
+//  buildnetflix
+//
+//  Created by Linh Dang on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct buildnetflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
